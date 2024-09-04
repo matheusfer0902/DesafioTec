cdd46cb02ad02c9e9dc562a81d78f70ee32358f2
Matheus Ferreira <matheusfer0902@gmail.com>
2024-09-04 18:43:31 -0300

    Solução questão 5

---
+++ questao5.py
@@ -0,0 +1,11 @@
+def inverter_string(s):
+    return s[::-1]
+
+# Exemplo de uso
+stringExemplo = "exemplo"
+stringInvertida = ""
+for char in stringExemplo:
+    stringInvertida = char + stringInvertida
+
+print("String original:", stringExemplo)
+print("String invertida:", stringInvertida)
